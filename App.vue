9dff4736ceb91648e53fb7a3516808bea6a16eec
dandan1129 <303078859@qq.com>
2020-01-10 15:27:52 +0800

    测试

--- App.vue
+++ App.vue
@@ -15,7 +15,7 @@
 <style lang="scss">
 	// 字体图标
 	@import './static/css/iconfont.css';
-	// 文本解析的样式
+	// 文本解析组件的样式
 	@import url("/components/gaoyia-parse/parse.css");
 	// 全局样式
 	@import './static/css/app.scss';
